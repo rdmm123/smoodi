4d831019565ed4055312279a7b976e87bf917103
Robert Martinez <robert.martinez@achievers.com>
2024-04-20 17:19:15 -0500

    Setup react router dom

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import App from 'App'
+import ErrorPage from 'error-page';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   const protocol = mode == 'development' ? 'http' : 'https'
   return {
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     root: './src/frontend',
     envDir: '../../',
     base: mode == 'development' ? '/' : '/dist',
