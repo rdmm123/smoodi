ac36a4798a0a6b9033e39681b8873561c6fa6e3d
Robert Martinez <robert.martinez@achievers.com>
2024-04-20 16:56:31 -0500

    Add tailwind css and remove unnecessary files

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,13 +1,12 @@
-import './App.css'
-
-function App() {
+export default function App() {
   return (
     <>
       <h1>Hello blendify!</h1>
       <h2><a href={BACKEND_HOST + '/auth/login'}>Log In</a></h2>
       <h2><a href={BACKEND_HOST + '/auth/logout'}>Log Out</a></h2>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
-
-export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/frontend/index.html",
+    "./src/frontend/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
