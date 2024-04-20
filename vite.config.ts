3413617d9a0a686990e3b4d3ff68c31c47f50f19
Robert Martinez <rdmarenco@uninorte.edu.co>
2024-04-20 12:21:41 -0500

    Set backend host as global constant

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  root: './src/frontend',
-  envDir: '../../',
-  server: {
-    host: true,
-    port: 5001,
-    strictPort: true,
-    watch: {
-      ignored: [
-        './src/*.py',
-        './src/**/*.py'
-      ]
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [react()],
+    root: './src/frontend',
+    envDir: '../../',
+    server: {
+      host: true,
+      port: 5001,
+      strictPort: true,
+      watch: {
+        ignored: [
+          './src/*.py',
+          './src/**/*.py'
+        ]
+      }
+    },
+    define: {
+      BACKEND_HOST: JSON.stringify(env.VITE_BACKEND_HOST)
     }
   }
 })
