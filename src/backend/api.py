50d433ed1bf0f6181778fedbe18d58f61b8be364
Robert Martinez <robert.martinez@achievers.com>
2024-04-21 23:16:48 -0500

    Refactor current user endpoint to make it more general

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,10 +1,42 @@
-from flask import Blueprint, session, current_app
+import json
+import datetime as dt
+from flask import Blueprint, session
+from dataclasses import asdict
+
+from core.client.spotify.spotify_client import SpotifyClient, SpotifyUser
+from core.storage.cache_storage import CacheStorage
+
+client = SpotifyClient()
+storage = CacheStorage()
 
 bp = Blueprint('api', __name__, url_prefix='/api')
-@bp.route('/current_user')
-def current_user():
-    current_user = session.get('email')
-    # TODO: Handle token refresh here
+
+@bp.route('/users/<user_email>')
+def get_user(user_email: str):
+    if user_email == 'me':
+        user_email = session.get('email')
+
+    user_raw = storage.read(f"user:{user_email}")
+
+    if not user_raw:
+        return {
+            'message': f'User {user_email} not found!'
+        }, 404
+    
+    user = json.loads(user_raw)
+    user_obj = SpotifyUser(**user)
+    new_auth_data = client.handle_token_refresh(user_obj.token,
+                                user_obj.token_expires,
+                                dt.datetime.fromisoformat(user_obj.token_expires))
+        
+    if new_auth_data['refreshed']:
+        user_obj.load_auth_data_from_response(new_auth_data)
+        storage.write(f'user:{user_obj.email}', json.dumps(asdict(user)))
+
     return {
-        'current_user': current_user
+        'user': asdict(user_obj)
     }
+
+@bp.route('')
+def user_session():
+    pass
