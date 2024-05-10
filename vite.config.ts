699c215c8e51eed036a4db840e4c8e2fbea5a015
Robert Martinez <rdmarenco@uninorte.edu.co>
2024-05-10 12:24:40 -0500

    Update project to only use env files for FE

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
-  const protocol = mode == 'development' ? 'http' : 'http'//'https'
+  const protocol = mode == 'development' ? 'http' : 'https'
   return {
     plugins: [react(), tsconfigPaths()],
     root: './src/frontend',
