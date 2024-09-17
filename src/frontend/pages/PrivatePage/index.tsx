d4de99c4353c89e042df6978c5d9073dd8719895
Robert Martinez <rdmarenco@uninorte.edu.co>
2024-09-17 19:49:58 -0300

    Fix private page bug

--- src/frontend/pages/PrivatePage/index.tsx
+++ src/frontend/pages/PrivatePage/index.tsx
@@ -4,14 +4,19 @@ import { useNavigate } from "react-router-dom";
 import { useCurrentUserQuery } from "hooks/user";
 
 export default function PrivatePage({ children }: { children: ReactNode }) {
-    const { data: user } =  useCurrentUserQuery();
+    const { data: user, isPending } =  useCurrentUserQuery();
     const navigate = useNavigate();
 
+
     useEffect(() => {
-        if (!user?.id) {
+        if (!user?.id && !isPending) {
             navigate('/');
         }
     })
+
+    if (isPending) {
+        return <p>Loading...</p>
+    }
 
     return <>{children}</>
 }
